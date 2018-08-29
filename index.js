79f3dfdfeb330acf1c8d34aea3e731c78e7e2641
YanC Lin <yonalin1@gmail.com>
2018-08-29 20:26:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
-class BoardMember(name, homeState, training){
-  this.name = name
-  this.homeState = homeState
-  this.training = training
-
-  BoardMember.prototype.veto = function(){
+class BoardMember{
+  constructor(name, homeState, training){
+  this.name = name;
+  this.homeState = homeState;
+  this.training = training;
+}
+     veto(){
     return 'No, I must disagree'
-  }
-  BoardMember.prototype.approve = function(){
+  };
+    approve(){
     return 'You can do that!'
-  }
-  BoardMember.prototype.doCharity = function(){
+  };
+    doCharity(){
     return "I like to help people."
-  }
+  };
 
-  BoardMember.prototype.releasePressStatement = function(){
+   releasePressStatement(){
     return "You will see great things from Scuber."
-  }
+  };
 
-  BoardMember.prototype.sayHi = function(){
+    sayHi(){
     return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
   }
 }
